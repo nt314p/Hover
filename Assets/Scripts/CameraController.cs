9c7d7809b2f19cfcd3e2964353d26acf57b6d9b3
Nicolas Tong <ntong@bayviewglen.ca>
2018-10-14 20:50:46 -0400

    Reimport for 2018.2.12

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,9 +6,11 @@ public class CameraController : MonoBehaviour {
 
 	GameObject player;
 
-	// smooth follow
+	// smooth follow variables
 	Vector3 target;
 	private Vector3 positionVelocity;
+    Vector3 offset = new Vector3(0, 26, -40);
+    float follow = 0.15f;
 
 	void Start () {
 		player = GameObject.FindGameObjectWithTag ("Player");
@@ -16,8 +18,9 @@ public class CameraController : MonoBehaviour {
 
 	void FixedUpdate () {
 		if (!Player.dead && !Player.powerLoss) {
-			target = player.transform.position + new Vector3 (0, 26, -50);
-			transform.position = Vector3.SmoothDamp(transform.position, target, ref positionVelocity, 0.15f);
+            
+			target = player.transform.position + offset;
+			transform.position = Vector3.SmoothDamp(transform.position, target, ref positionVelocity, follow);
 		}
 	}
 }
